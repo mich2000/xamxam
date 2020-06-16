1c69b5db876d4b2e3a23134751499c87c088e679
michael hertsens <michael.hertsens@student.odisee.be>
2020-06-16 23:30:54 +0200

    update

--- PID/updater.ps1
+++ PID/updater.ps1
@@ -1,4 +1,4 @@
-markdown-pdf .\PID\pid.md .\logboek.md -s .\github.css -o documentatie.pdf
+markdown-pdf .\pid.md .\logboek.md -s .\github.css -o documentatie.pdf
 
 git add .
 git commit -m $args[0]
